ddb6e7998846968849ff6399547a04fa01637b8f
abhivashist <vashistabhishek21@gmail.com>
2023-01-20 14:44:10 +0530

    multipleCatch_Demo

---
+++ src/multipleCatch_Demo.java
@@ -0,0 +1,16 @@
+public class multipleCatch_Demo {
+
+    public static void main(String args[]){
+        try{
+            int a[]=new int[5];
+            a[5]=30/0;
+        }
+        catch(ArithmeticException e)
+        {System.out.println("task1 is completed");}
+        catch(ArrayIndexOutOfBoundsException e)
+        {System.out.println("task 2 completed");}
+        catch(Exception e)
+        {System.out.println("task 3 completed");}
+        System.out.println("remaining code");
+    }
+}
