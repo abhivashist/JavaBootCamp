100fc59d13da1f9f33681051156439225a4cede1
abhivashist <vashistabhishek21@gmail.com>
2023-01-20 11:36:07 +0530

    try-Catch

---
+++ src/tryCatch_Demo.java
@@ -0,0 +1,15 @@
+public class tryCatch_Demo {
+    public static void main(String[] args) {
+
+        try {
+
+            // code that generate exception
+            int divideByZero = 5 / 0;
+            System.out.println("Rest of code in try block");
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("ArithmeticException => " + e.getMessage());
+        }
+    }
+}
