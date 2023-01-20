35828dda6ceff4f299ca4e865d4e4de8ffc56fb1
abhivashist <vashistabhishek21@gmail.com>
2023-01-20 14:45:54 +0530

    Finally_Demo

---
+++ src/Finally_Demo.java
@@ -0,0 +1,12 @@
+public class Finally_Demo {
+    public static void main(String args[]){
+        try{
+            int data=55/5;
+            System.out.println(data);
+        }
+        catch(NullPointerException e)
+        {System.out.println(e);}
+        finally {System.out.println("finally block is executed");}
+        System.out.println("remaining code");
+    }
+}
