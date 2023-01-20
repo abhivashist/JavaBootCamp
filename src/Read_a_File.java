c2fc980ee430fc7060f50825c223bc0f3fc04b65
abhivashist <vashistabhishek21@gmail.com>
2023-01-20 15:16:17 +0530

    read a file

---
+++ src/Read_a_File.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Read_a_File {
+    public static void main(String[] args) {
+
+        File myFile = new File("JavaDemo");
+        try {
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
